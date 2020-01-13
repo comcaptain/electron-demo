73d9c0b0ffc663053f713348b4b71c86734c6c05
Tony <htmlcaptain@gmail.com>
2020-01-13 18:37:46 +0900

    Add basic golden-layout

--- src/index.js
+++ src/index.js
@@ -2,11 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import GoldenLayout from 'golden-layout';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+window.ReactDOM = ReactDOM;
+window.React = React;
+let myLayout = new GoldenLayout({
+    content: [{
+        type: 'row',
+        content:[{
+            type:'react-component',
+            component: 'app',
+            props: { label: 'A' }
+        },{
+            type: 'column',
+            content:[{
+                type:'react-component',
+                component: 'app',
+                props: { label: 'B' }
+            },{
+                type:'react-component',
+                component: 'app',
+                props: { label: 'C' }
+            }]
+        }]
+    }]
+});
+myLayout.registerComponent("app", App);
+myLayout.init();
